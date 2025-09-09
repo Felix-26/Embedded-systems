bb7b2d694bca42f4272609c837aadc6fe81f6a13
Felix Thomas <felixthomas173@gmail.com>
2025-09-09 13:03:27 +0530

    H.W

---
+++ AVR/Blink_LED_PD3/Blink_LED_PD3/main.c
@@ -0,0 +1,26 @@
+/*
+ * Blink_LED_PD3.c
+ *
+ * Created: 9/9/2025 12:40:12 PM
+ * Author : Felix Thomas
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	// set PD3 as OUTPUT.
+    DDRD = (1<<3);
+    while (1) 
+    {
+		// set PD3 HIGH
+		PORTD |= (1<<3);
+		_delay_ms(500);
+		// set PD3 LOW
+		PORTD &= ~(1<<3);
+		_delay_ms(500); 
+    }
+}
+
